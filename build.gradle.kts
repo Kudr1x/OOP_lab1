53512744278294abd54744317af5fe5899f44faf
kudrix <max51kud@gmail.com>
2024-10-29 12:21:21 +0300

    add wiki request, serialization and print request todo open browser

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     kotlin("jvm") version "2.0.20"
+    kotlin("plugin.serialization") version "1.4.21"
 }
 
 group = "org.example"
@@ -12,6 +13,13 @@ repositories {
 dependencies {
     testImplementation(kotlin("test"))
     implementation("io.ktor:ktor-client-core:3.0.0")
+    implementation("io.ktor:ktor-client-cio:3.0.0")
+    implementation("io.ktor:ktor-client-serialization:3.0.0")
+    implementation("io.ktor:ktor-client-logging:3.0.0")
+    implementation("ch.qos.logback:logback-classic:1.4.12")
+    implementation("io.ktor:ktor-serialization-kotlinx-json:3.0.0")
+    implementation("io.ktor:ktor-server-content-negotiation:3.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.0")
 }
 
 tasks.test {
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,69 @@
-package org.example
+import io.ktor.client.*
+import io.ktor.client.request.*
+import io.ktor.client.statement.*
+import io.ktor.client.engine.cio.*
+import io.ktor.client.plugins.logging.*
+import kotlinx.coroutines.runBlocking
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.json.Json
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
+
+@Serializable
+data class WikiSearchResponse(
+    val query: Query
+)
+
+@Serializable
+data class Query(
+    val search: List<SearchResult>,
+)
+
+@Serializable
+data class SearchResult(
+    val title: String,
+    val snippet: String
+)
+
+private val json = Json {
+    ignoreUnknownKeys = true
+}
+
+fun removeHtmlTags(input: String): String {
+    return input.replace(Regex("<[^>]*>"), "")
+}
 
 fun main() {
-    println("Hello World!")
+    runBlocking {
+        val client = HttpClient(CIO) {
+            install(Logging) {
+                level = LogLevel.INFO
+            }
+        }
+
+        print("Введите запрос: ")
+        val inp = readLine()?.trim() ?: ""
+
+        val encodedSearch = URLEncoder.encode(inp, StandardCharsets.UTF_8)
+
+        try {
+            val response: HttpResponse = client
+                .get("https://ru.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch=\"${encodedSearch}\"") {
+            }
+
+            val responseBody = response.bodyAsText()
+
+            val searchResult = json.decodeFromString<WikiSearchResponse>(responseBody)
+            
+            for (result in searchResult.query.search) {
+                val cleanSnippet = removeHtmlTags(result.snippet)
+                println("Title: ${result.title}")
+                println("Snippet: $cleanSnippet\n")
+            }
+        } catch (e: Exception) {
+            println("Error occurred: ${e.message}")
+        } finally {
+            client.close()
+        }
+    }
 }
