c8a49769ffd5591aefcff640b680af10a47c80aa
kudrix <max51kud@gmail.com>
2024-11-08 13:47:21 +0300

    add check not correct input

--- src/main/kotlin/controller/WikiController.kt
+++ src/main/kotlin/controller/WikiController.kt
@@ -9,15 +9,26 @@ class WikiController(private val wikiService: WikiServiceInterface) : WikiContro
     override suspend fun performSearch(query: String) {
         val searchResult: WikiSearchResponse = wikiService.search(query)
 
+        var i = 0;
         for (result in searchResult.query.search) {
+            i++
             val cleanSnippet = StringUtils.removeHtmlTags(result.snippet)
-            println("Title: ${result.title}")
+            println("$i) Title: ${result.title}")
             println("Snippet: $cleanSnippet")
             println("Page ID: ${result.pageid}")
             println()
         }
 
-        val urlToOpen = "https://ru.wikipedia.org/w/index.php?curid=${searchResult.query.search[0].pageid}"
+        print("Какую страницу отркыть?: ")
+
+        var input: Int = readlnOrNull()?.toInt() ?: 0
+        while ((input <= 0) or (input > 10)) {
+            println("Не корректное значение")
+            print("Какую страницу отркыть?: ")
+            input = readlnOrNull()?.toInt() ?: 0
+        }
+
+        val urlToOpen = "https://ru.wikipedia.org/w/index.php?curid=${searchResult.query.search[input-1].pageid}"
         openUrlInBrowser(urlToOpen)
     }
 }
--- src/main/kotlin/service/WikiService.kt
+++ src/main/kotlin/service/WikiService.kt
@@ -12,7 +12,9 @@ import java.nio.charset.StandardCharsets
 class WikiService(private val client: HttpClient, private val json: Json) : WikiServiceInterface {
     override suspend fun search(query: String): WikiSearchResponse {
         val encodedQuery = URLEncoder.encode(query, StandardCharsets.UTF_8)
-        val response: HttpResponse = client.get("https://ru.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch=\"$encodedQuery\"") {
+        val response: HttpResponse = client.get(
+            "https://ru.wikipedia.org/w/api.php?action=query&list=search&utf8=&format=json&srsearch=\"$encodedQuery\""
+        ) {
             contentType(ContentType.Application.Json)
         }
 
